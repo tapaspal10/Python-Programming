1b561111ccd1f46ddfc28f729aa32f355d1543de
sarmisthapal10 <73027162+sarmisthapal10@users.noreply.github.com>
2022-10-30 23:00:36 +0530

    Exception Handling

---
+++ exception-handling.py
@@ -0,0 +1,13 @@
+# Program to depict else clause with try-except
+
+def AbyB(a , b):
+	try:
+		c = ((a+b) / (a-b))
+	except ZeroDivisionError:
+		print ("a/b result in 0")
+	else:
+		print (c)
+
+# Driver program to test above function
+AbyB(2.0, 3.0)
+AbyB(3.0, 3.0)
